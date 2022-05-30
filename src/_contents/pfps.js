cddf1df00ce808ff8b638d0abd4dc614a6a2bda5
Charles Thibault <charles.thibault@agencesudo.ca>
2022-05-30 00:26:59 -0400

    pfp vaults

---
+++ src/_contents/pfps.js
@@ -0,0 +1,52 @@
+export default [
+  {
+    title: 'M.N.G.O #2165',
+    image: '/images/ipfs/bafybeigig4plgfgwhddxjuqkm5honc3b5icn7paba3sj2fr756ll22f3xi.png',
+    link: 'https://www.randgallery.com/algo-collection/?address=679065729',
+  },
+  {
+    title: 'Last G #166',
+    image: '/images/ipfs/bafkreifix7qqkbtqpil7phul22aa3f2j2z3ssoadrdqj23b7zpilfkaakq.png',
+    link: 'https://www.randgallery.com/algo-collection/?address=678288557',
+  },
+  {
+    title: 'M.N.G.O #2920',
+    image: '/images/ipfs/bafybeih5yr523rmptjmz5zjylciyxxc3orpatjsl55z7nd5ll2bbtjdoau.png',
+    link: 'https://www.randgallery.com/algo-collection/?address=679066648',
+  },
+  {
+    title: 'Algo Hands #430',
+    image: '/images/ipfs/bafybeib7ezk343cfqcekv77rblc7ce7k7bw2xzeub7n6dribx25x72eetu.png',
+    link: 'https://www.randgallery.com/algo-collection/?address=684299559',
+  },
+  {
+    title: 'Little Freak #338',
+    image: '/images/ipfs/bafybeib53ynhtrobfabe5bdjbhlegob32ppifrkz6gc6shf2etg4nrrsi4.png',
+    link: 'https://www.randgallery.com/algo-collection/?address=688774568',
+  },
+  {
+    title: 'Algo Hands #40',
+    image: '/images/ipfs/bafybeigdwwhjdcaygeysvukclxvhqmwlilb2gahbkus6a2vnmly6fklb7m.png',
+    link: 'https://www.randgallery.com/algo-collection/?address=684298566',
+  },
+  {
+    title: 'APP #63',
+    image: '/images/ipfs/bafybeid5ueasv7ta23kk7cbiyhegum5o3jwbppku7ticc2sfixvt2hqvv4.png',
+    link: 'https://www.randgallery.com/algo-collection/?address=680304322',
+  },
+  {
+    title: 'AOWL #933',
+    image: '/images/ipfs/bafybeiffhty5nizc7yf2kpa4bxwrrxvt2uex7clburdldbtpgmhjqv42ty.png',
+    link: 'https://www.randgallery.com/algo-collection/?address=679053261',
+  },
+  {
+    title: 'APP #48',
+    image: '/images/ipfs/bafybeibsh2ldbg5qfyi5qkhbncvoe5xm2o73suojkql26gmz4zqpkjqdx4.png',
+    link: 'https://www.randgallery.com/algo-collection/?address=680305034',
+  },
+  {
+    title: 'Smokey #000',
+    image: '/images/ipfs/bafybeie73ese4rzwghpusl7wuo3wt772ywypx4mgtfsmeryu7qjw6hxtxy.png',
+    link: 'https://www.randgallery.com/algo-collection/?address=688775209',
+  },
+];
---
+++ src/_contents/vault-pfps.md
@@ -0,0 +1,5 @@
+Below are PFP representations of the NFTs we hold in our vault.
+
+These PFPs have clawback and freeze enabled to avoid any malicious activity with them, and to secure the use of them as PFPs only
+
+If you hold Angel Wings, feel free to opt into the PFP(s) you wish to use, and reach out to us on Discord/Telegram to claim them!
